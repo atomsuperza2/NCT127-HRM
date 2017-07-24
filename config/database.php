d4a5bd2e1b1d363840b4166e82909f1fb61f8205
Atomsuperza1234 <atomsuperza2@gmail.com>
2017-07-24 15:59:53 +0700

    commit

--- config/database.php
+++ config/database.php
@@ -14,7 +14,7 @@ return [
     */
 
     // 'default' => env('DB_CONNECTION', 'mysql'),
-    'default' => 'pgsql',
+    'default' => 'heroku',
 
     /*
     |--------------------------------------------------------------------------
@@ -68,16 +68,16 @@ return [
         //     'sslmode' => 'prefer',
         // ],
 
-        'pgsql' => [
-            'driver'   => 'pgsql',
-            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
-            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-            'username' => parse_url(getenv("DATABASE_URL"))["user"],
-            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
-            'charset'  => 'utf8',
-            'prefix'   => '',
-            'schema'   => 'public',
-        ],
+        'heroku' => [
+      'driver'   => 'pgsql',
+      'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+      'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+      'username' => parse_url(getenv("DATABASE_URL"))["user"],
+      'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+      'charset'  => 'utf8',
+      'prefix'   => '',
+      'schema'   => 'public',
+      ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
