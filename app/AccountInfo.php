f7cc448d9f64488d178739a8000c6403de2491bc
Atomsuperza1234 <atomsuperza2@gmail.com>
2017-07-24 17:50:40 +0700

    commit db

--- app/AccountInfo.php
+++ app/AccountInfo.php
@@ -11,7 +11,7 @@ class AccountInfo extends Model
   use Notifiable;
   use HasRoles;
 
-  protected $table = 'accountinfo';
+  protected $table = 'accountinfos';
 
   protected $guarded = ['username', 'password'];
 
